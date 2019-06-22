d8c37093a70f8baefbd8c1dd4a79f0a6f5311f46
Arthur Siqueira <arthur.siq5@gmail.com>
2019-06-21 21:48:00 -0300

    adicionadas demais opções de tela

--- src/src/display/Display.java
+++ src/src/display/Display.java
@@ -14,9 +14,14 @@ import javax.swing.JFrame;
 public class Display {
     private JFrame frame;
     
+    /**
+     * 
+     * @param titulo 
+     */
     public Display(String titulo){
         this.frame = new JFrame(titulo);
-        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setLocationRelativeTo(null); // a posição da janela não é atrelada a nenhum objeto da tela
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // se clicar em "exit", fecha a aplicação inteira
         this.frame.setVisible(true);
     }
 }

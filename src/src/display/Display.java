a418d8344b8c0648d4b3891b81269142b61c8f30
Arthur Siqueira <arthur.siq5@gmail.com>
2019-06-21 22:10:05 -0300

    definidas opções de dimensionamento da tela

--- src/src/display/Display.java
+++ src/src/display/Display.java
@@ -24,14 +24,18 @@ public class Display {
      * @param altura 
      */
     public Display(String titulo, int largura, int altura){
+        // inicializando objetos
         this.canvas = new Canvas();
+        this.frame = new JFrame(titulo);
         
         // define o tamanho da tela do jogo
         this.canvas.setPreferredSize(new Dimension(largura, altura));
         this.canvas.setMaximumSize(new Dimension(largura, altura));
         this.canvas.setMinimumSize(new Dimension(largura, altura));
         
-        this.frame = new JFrame(titulo);
+        this.frame.add(this.canvas); // passando canvas pro objeto
+        this.frame.pack(); // puxa os dados de tamanho do componente interno
+        
         this.frame.setLocationRelativeTo(null); // a posição da janela não é atrelada a nenhum objeto da tela
         this.frame.setResizable(Boolean.FALSE); // não pode ser redimensionada pelo usuário
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // se clicar em "exit", fecha a aplicação inteira

5081993b7b2d130931343f714a68d8f1250b0722
Arthur Siqueira <arthur.siq5@gmail.com>
2019-08-09 10:57:41 -0300

    adicionado menu primitivo

--- src/src/estados/MenuEstado.java
+++ src/src/estados/MenuEstado.java
@@ -16,12 +16,17 @@ import src.utils.Constantes;
  */
 public class MenuEstado implements Estado {
     private Font fontePrincipal;
+    private Font fonteOpcoes;
     private String nomeDoJogo;
-
+    private String[] opcoes = {"START", "HELP", "EXIT"};
+    private int opcaoSelecionada;
+    
     @Override
     public void init() {
         this.fontePrincipal = new Font("Dialog", Font.PLAIN, 48);
+        this.fonteOpcoes    = new Font("Dialog", Font.PLAIN, 24);
         this.nomeDoJogo = "PONG";
+        this.opcaoSelecionada = 0;
     }
 
     @Override
@@ -42,9 +47,24 @@ public class MenuEstado implements Estado {
         );
         grafico.setColor(Color.WHITE);
         grafico.setFont(this.fontePrincipal);
-        grafico.drawString(this.nomeDoJogo, (int) (largura/2.5), 
-                altura/3
+        grafico.drawString(
+                this.nomeDoJogo, 
+                (int) (largura/2.5), 
+                (int) altura/3
         );
+        
+        grafico.setFont(this.fonteOpcoes);
+        for (int i = 0; i < this.opcoes.length; i++) {
+            grafico.setColor(Color.WHITE);
+            if(i == this.opcaoSelecionada){
+                grafico.setColor(Color.YELLOW);
+            }
+            grafico.drawString(
+                this.opcoes[i], 
+                (int) (largura/2) - grafico.getFontMetrics().stringWidth(this.opcoes[i]), 
+                (int) (altura * (0.75)) + (grafico.getFontMetrics(this.fonteOpcoes).getHeight() * i)
+            );
+        }
     }
 
     @Override
